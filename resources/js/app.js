0f678cbfb1c22677a6eb623c0759e01c5d3901e2
aronei44 <meliodasmeliodas224@gmail.com>
2021-11-02 19:33:08 +0700

    menambahkan progress bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress';
+
+
+InertiaProgress.init({
+  color: '#ED8936',
+  showSpinner: true
+});
+
 
 const app = document.getElementById('app')
 
