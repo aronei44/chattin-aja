ad58652acb57408ebd955a47c07242ce6eaed112
al barkah <albarkahalkahfi05@gmail.com>
2021-11-07 06:33:36 +0700

    fix bug

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class LogController extends Controller
